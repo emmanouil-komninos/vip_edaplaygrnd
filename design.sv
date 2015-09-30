21a30d6d4730e67ff802c2f9320edeb72efb77f3
Emmanouil Komninos <komninos@kth.se>
2015-09-30 10:45:00 +0200

    Update design.sv

--- design.sv
+++ design.sv
@@ -1,46 +1,49 @@
 module _design (design_if _if);
   
-  reg [31:0] data;
+ 
+  reg [31:0] ctrl; // [31:24] op_code [23:16] address [15:0] data
   
-  reg [7:0] op_code;
-  reg [7:0] address;
-  
-  reg [31:0] reg_data;
-  reg [31:0] reg_array [7:0];
+  reg [15:0] reg_array [7:0];
+  reg [15:0] reg_data;
   
   always @(posedge _if.clk)
     begin
-      address <= _if.address;
-      op_code <= _if.op_code;
-      data <= _if.data;
+      ctrl[23:16] <= _if.address;
+      ctrl[31:24] <= _if.op_code;
+      ctrl[15:0] <= _if.data;
     end
   
-  always @(op_code)
+  always @(ctrl)
     begin
       
-      case(op_code)
+      case(ctrl[31:24])
         1 : 
           begin
-            reg_data <= data;
+            reg_array[ctrl[23:16]] <= ctrl[15:0];
+            reg_data <= ctrl[15:0];
             $display("op_code is 1");
           end
         2 : 
           begin
-            reg_data <= data>>1;
+            reg_array[ctrl[23:16]] <= ctrl[15:0]>>1;
+            reg_data <= ctrl[15:0]>>1;
             $display("op_code is 2");
           end
         3 : 
           begin
-            reg_data <= data<<1;
+            reg_array[ctrl[23:16]] <= ctrl[15:0]<<1;
+            reg_data <= ctrl[15:0]<<1;
             $display("op_code is 3");
           end
         4 : 
           begin
-            reg_data <= ~data;
+            reg_array[ctrl[23:16]] <= ~ctrl[15:0];
+            reg_data <= ~ctrl[15:0];
             $display("op_code is 4");
           end
         default : 
           begin
+            reg_array[ctrl[23:16]] <= reg_array[ctrl[23:16]];
             reg_data <= reg_data;
             $display("op_code is not 1");
           end
