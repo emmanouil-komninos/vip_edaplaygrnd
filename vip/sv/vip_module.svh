d8e9db00b89c3584b2be8c2d66907585f33ac18a
Emmanouil Komninos <komninos@kth.se>
2015-09-30 22:13:57 +0200

    Update vip_module.svh

--- vip/sv/vip_module.svh
+++ vip/sv/vip_module.svh
@@ -29,3 +29,18 @@ class vip_module_uvc extends uvm_env;
   extern function void connect_phase (uvm_phase phase);
   
 endclass
+    
+    function void vip_module_uvc::build_phase(uvm_phase phase);
+      super.build_phase(phase);
+      reg2cbus = vip_reg2cbus_adapter::type_id::create("reg2cbus");
+      cbus_predictor = uvm_reg_predictor#(vip_base_seq_item)::type_id::create(
+        "cbus_predictor", this);      
+    endfunction
+    
+
+    function void vip_module_uvc::connect_phase(uvm_phase phase);
+      super.connect_phase(phase);
+      // connect adapter  and register map to predictor
+      cbus_predictor.map = vip_reg_model.default_map;
+      cbus_predictor.adapter = reg2cbus;
+    endfunction
