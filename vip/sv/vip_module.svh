e98043e8ad8ad017a9b3d9b7079e479d4721a4f4
Emmanouil Komninos <komninos@kth.se>
2015-09-30 14:58:48 +0200

    Update vip_module.svh

--- vip/sv/vip_module.svh
+++ vip/sv/vip_module.svh
@@ -35,24 +35,26 @@ endclass
 
 
 // register file definition
-class vip_rf_c extends uvm_reg_block;
+class vip_register_file_c extends uvm_reg_block;
   
   // register members
   rand ctrl_reg_c ctrl_reg;
   
-  `uvm_object_utils(vip_rf_c)
+  `uvm_object_utils(vip_register_file_c)
   
-  function new (string name = "vip_rf_c");
+  function new (string name = "vip_register_file_c");
     super.new(name, UVM_NO_COVERAGE);
   endfunction
   
-  virtual function build();
+  virtual function void build();
     // create, configure, build each register
     ctrl_reg = ctrl_reg_c::type_id::create("ctrl_reg");
+    
+    // parent, reg_file parent, path
     ctrl_reg.configure(this, null, "ctrl");
     ctrl_reg.build();
     
-    // define address mappings
+    // define register address mappings
     default_map = create_map("default_map", 
                              0, 4, UVM_LITTLE_ENDIAN, 0);
     default_map.add_reg(ctrl_reg, 0, "RW"); // 0 offset from base
@@ -62,3 +64,36 @@ endclass
 
 
 // register model definition
+class vip_reg_model_c extends uvm_reg_block;
+  
+  vip_register_file_c vip_rf;
+  
+  `uvm_object_utils(vip_reg_model_c)
+  
+  function new( string name = "vip_register_model_c");
+    super.new(name, UVM_NO_COVERAGE);
+  endfunction
+  
+  virtual function void build();
+    // create, configure register file
+    vip_rf = vip_register_file_c::type_id::create("vip_rf");
+    
+    // parent, path
+    vip_rf.configure(this, "regs");
+    vip_rf.build();
+    vip_rf.lock_model();
+    
+    // define register file address mappings
+    default_map = create_map("default_map", 
+                             0, 4, UVM_LITTLE_ENDIAN, 0);
+    
+    default_map.add_supmap(vip_rf.default_map, 0);
+    default_map.set_check_on_read();
+    
+    set_hdl_path_root("tb_top.dut");
+    this.lock_model();
+    
+  endfunction
+  
+endclass
+  
