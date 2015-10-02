1986edf7f3ea3584b64ed9ec0fc441ef4826ba0e
Emmanouil Komninos <komninos@kth.se>
2015-10-02 12:56:22 +0200

    Update design.sv

--- design.sv
+++ design.sv
@@ -6,11 +6,20 @@ module _design (design_if _if);
   reg [15:0] reg_array [7:0];
   reg [15:0] reg_data;
   
-  always @(posedge _if.clk)
+  always @(posedge _if.clk, _if.reset)
     begin
-      ctrl[23:16] <= _if.address;
-      ctrl[31:24] <= _if.op_code;
-      ctrl[15:0] <= _if.data;
+      if (_if.reset)
+        begin
+          ctrl[23:16] <= 'hff;
+          ctrl[31:24] <= 'hff;
+          ctrl[15:0] <= 'hffff;
+        end
+      else
+        begin
+          ctrl[23:16] <= _if.address;
+          ctrl[31:24] <= _if.op_code;
+          ctrl[15:0] <= _if.data;
+        end
     end
   
   always @(ctrl)
