1319b76f8b35c66790d8bc3480f2d8382d11f2f4
Emmanouil Komninos <komninos@kth.se>
2015-10-04 21:16:07 +0200

    Update design.sv

--- design.sv
+++ design.sv
@@ -21,43 +21,5 @@ module _design (design_if _if);
           ctrl[15:0] <= _if.data;
         end
     end
-  
-  always @(ctrl)
-    begin
-      
-      case(ctrl[31:24])
-        1 : 
-          begin
-            reg_array[ctrl[23:16]] <= ctrl[15:0];
-            reg_data <= ctrl[15:0];
-            $display("op_code is 1");
-          end
-        2 : 
-          begin
-            reg_array[ctrl[23:16]] <= ctrl[15:0]>>1;
-            reg_data <= ctrl[15:0]>>1;
-            $display("op_code is 2");
-          end
-        3 : 
-          begin
-            reg_array[ctrl[23:16]] <= ctrl[15:0]<<1;
-            reg_data <= ctrl[15:0]<<1;
-            $display("op_code is 3");
-          end
-        4 : 
-          begin
-            reg_array[ctrl[23:16]] <= ~ctrl[15:0];
-            reg_data <= ~ctrl[15:0];
-            $display("op_code is 4");
-          end
-        default : 
-          begin
-            reg_array[ctrl[23:16]] <= reg_array[ctrl[23:16]];
-            reg_data <= reg_data;
-            $display("op_code is not 1");
-          end
-      endcase
-    end
 
 endmodule
-            
