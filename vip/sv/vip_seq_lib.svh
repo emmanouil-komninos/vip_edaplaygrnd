e7fa5170ea5cf22f8629d09f73bd925e17b8ddbd
Emmanouil Komninos <komninos@kth.se>
2015-10-07 10:04:16 +0200

    Update vip_seq_lib.svh

--- vip/sv/vip_seq_lib.svh
+++ vip/sv/vip_seq_lib.svh
@@ -25,10 +25,32 @@ class vip_simple_reg_seq extends base_reg_seq;
   function new (string name = "vip_simple_reg_seq");
     super.new(name);
   endfunction
+  // pre_body for init_done
   
   virtual task body();
     uvm_status_e status;
+    bit [31:0] rd_val;
+    string paths[$];
     `uvm_info("VIP_SIMPLE_REG_SEQ", "Running", UVM_LOW)
-    reg_model.vip_rf.ctrl_reg.write(status, 'h12345);
+
+    reg_model.vip_rf.ctrl.print();
+    
+    // frontdoor write
+    reg_model.vip_rf.ctrl.write(status, 'h0);
+    
+    #50ns;
+    
+    // backdoor write
+    poke_reg(reg_model.vip_rf.ctrl, status, 'h12345);
+    
+    #50ns;
+    
+    // backdoor read
+    peek_reg(reg_model.vip_rf.ctrl, status, rd_val );
+    assert(rd_val == 'h12345);
+    
+    reg_model.vip_rf.ctrl.print();
+    `uvm_info("VIP_SIMPLE_REG_SEQ", 
+              $sformatf("Read value is %d", rd_val), UVM_LOW)
   endtask
 endclass
