3a4ea8872732c0eb8e8743cf35421d04872fd1d1
Emmanouil Komninos <komninos@kth.se>
2015-10-12 12:12:39 +0200

    Update vip_test.svh

--- vip/sv/vip_test.svh
+++ vip/sv/vip_test.svh
@@ -1,3 +1,115 @@
+// sequence classes
+class vip_simple_sequence extends vip_base_sequence;
+  
+  `uvm_object_utils(vip_simple_sequence)
+  
+  function new (string name = "vip_simple_sequence");
+    super.new(name);
+    set_automatic_phase_objection(1);
+  endfunction
+    
+  virtual task body();
+    `uvm_info("VIP_SIMPLE_SEQUENCE", "Running", UVM_LOW)
+    repeat(70)
+      begin
+        `uvm_do_with(req, {req.op_code inside {[1:2]};})  
+      end
+  endtask    
+endclass
+
+// register sequence
+class vip_simple_reg_seq extends base_reg_seq;
+  
+  `uvm_object_utils(vip_simple_reg_seq)
+  
+  function new (string name = "vip_simple_reg_seq");
+    super.new(name);
+    set_automatic_phase_objection(1);
+  endfunction
+    
+  virtual task body();
+    uvm_status_e status;
+    bit [31:0] rd_val;
+    string paths[$];
+    `uvm_info("VIP_SIMPLE_REG_SEQ", "Running", UVM_LOW)
+
+    reg_model.vip_rf.ctrl.print();
+    
+    // frontdoor write
+    reg_model.vip_rf.ctrl.write(status, 'h0);
+    
+    #50ns;
+    
+    // backdoor write
+    poke_reg(reg_model.vip_rf.ctrl, status, 'h12345);
+    
+    #50ns;
+    
+    // backdoor read
+    peek_reg(reg_model.vip_rf.ctrl, status, rd_val );
+    assert(rd_val == 'h12345);
+    
+    reg_model.vip_rf.ctrl.print();
+    `uvm_info("VIP_SIMPLE_REG_SEQ", 
+              $sformatf("Read value is %d", rd_val), UVM_LOW)
+  endtask
+endclass
+
+// test virtual sequences
+class simple_vseq extends base_vseq;
+  
+  `uvm_object_utils(simple_vseq)
+  
+  vip_base_sequence req;
+  
+  function new (string name = "simple_vseq");
+    super.new(name);
+    set_automatic_phase_objection(1);
+  endfunction 
+    
+  virtual task body();    
+    req = vip_base_sequence::type_id::create("req");
+    if (req.get_automatic_phase_objection())
+      begin
+        `uvm_info($sformatf("%s", this.get_name()), 
+                  "Automatic phase objection is set", UVM_LOW)
+      end
+    `uvm_do_on(req, p_sequencer.seqr)    
+  endtask
+  
+endclass
+
+// test virtual sequence with reg_seq
+class simple_reg_vseq extends base_vseq;
+  
+  `uvm_object_utils(simple_reg_vseq)
+  
+  vip_simple_reg_seq reg_req;
+  
+  function new (string name = "simple_reg_vseq");
+    super.new(name);
+    set_automatic_phase_objection(1);
+  endfunction 
+    
+  virtual task body();
+    reg_req = vip_simple_reg_seq::type_id::create("reg_req");  
+    `uvm_do_on(reg_req, p_sequencer.seqr)    
+  endtask
+  
+endclass
+
+// test virtual seq calling c
+class simple_c_vseq extends base_vseq;
+  
+  `uvm_object_utils(simple_c_vseq)
+  
+  function new (string name = "simple_c_vseq");
+    super.new(name);
+  endfunction
+  
+endclass
+
+// test classes
 class vip_base_test extends uvm_test;
   
   // configuation. Automation
@@ -24,7 +136,6 @@ class vip_base_test extends uvm_test;
   // end of elaboration
   virtual function void end_of_elaboration();
     print();
-    //tb.reg_model.set_hdl_path_root("$root.tb_top.dut");
   endfunction
   
   // all dropped with drain time
@@ -44,7 +155,7 @@ class vip_base_test extends uvm_test;
                   $sformatf("get_objection_total=%0d",
                             objection.get_objection_total), 
                   UVM_LOW)
-        repeat(15) @(this.tb.env.m_config.vif.vip_tb_mod.tb_ck);
+        repeat(3) @(this.tb.env.m_config.vif.vip_tb_mod.tb_ck);
       end
   endtask
     
@@ -52,8 +163,6 @@ endclass
 
       
 // tests
-
-
 class simple_test extends vip_base_test;
   
   // configuation. Automation
@@ -76,17 +185,19 @@ class simple_test extends vip_base_test;
   endfunction
   
   // build phase
+  virtual function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    //vip_base_sequence::type_id::set_type_override(
+      //vip_simple_sequence::get_type());
+    set_type_override_by_type(vip_base_sequence::get_type(), 
+                            vip_simple_sequence::get_type());   
+  endfunction
+  
   // end of elaboration
   
   // run phase
   virtual task run_phase(uvm_phase phase);
-    //`uvm_info($sformatf("%s", this.get_name()), 
-              //$sformatf("get_objection_count=%0d", 
-                        //phase.get_objection_count(this)), UVM_LOW) 
-    phase.raise_objection(this, "test is raising an ojection");
-    //`uvm_info($sformatf("%s", this.get_name()), 
-              //$sformatf("get_objection_count=%0d", 
-                        //phase.get_objection_count(this)), UVM_LOW)
+    //phase.raise_objection(this, "test is raising an ojection");
     
     // find the sequencer
     //uvm_component seqr;
@@ -96,86 +207,38 @@ class simple_test extends vip_base_test;
     // create the sequence
     base_sequence = vip_base_sequence::type_id::create("base_sequence");
     
+    // call the following to enable starting_phase.raise/drop_objection(..)
+    base_sequence.set_starting_phase(phase); // uvm-1.2
+    
     // start it on the sequencer
     base_sequence.start(tb.env.agent.seqr);
-    
-    #20 phase.drop_objection(this, "test is dropping the objection");
-    //`uvm_info($sformatf("%s", this.get_name()), 
-              //$sformatf("get_objection_count=%0d", 
-                        //phase.get_objection_count(this)), UVM_LOW) 
+
+    //#20 phase.drop_objection(this, "test is dropping the objection");
   endtask
   
 endclass
 
-
-// test virtual sequence
-class simple_vseq extends base_vseq;
-  
-  `uvm_object_utils(simple_vseq)
-  
-  vip_base_sequence req;
-  
-  function new (string name = "simple_vseq");
-    super.new(name);
-  endfunction 
-    
-  task body();
-    req = vip_base_sequence::type_id::create("req");    
-    `uvm_do_on(req, p_sequencer.seqr)    
-  endtask
-  
-endclass
-
-// test virtual sequence with reg seq
-class simple_reg_vseq extends base_vseq;
-  
-  `uvm_object_utils(simple_reg_vseq)
-  
-  vip_simple_reg_seq reg_req;
-  
-  function new (string name = "simple_reg_vseq");
-    super.new(name);
-  endfunction 
-    
-  task body();
-    reg_req = vip_simple_reg_seq::type_id::create("reg_req");   
-    `uvm_do_on(reg_req, p_sequencer.seqr)    
-  endtask
-  
-endclass
-
-class vseq_simple_test extends vip_base_test;
+class vseq_base_test extends vip_base_test;
   
   // configuation. Automation
   // class members. Automation
-  base_vseq vseq;
   
   // macro for factory registeration and automation
   // of class members
-  `uvm_component_utils(vseq_simple_test)
+  `uvm_component_utils(vseq_base_test)
   
-  // test's components. No automation      
+  // test's components. No automation
+  base_vseq vseq;     
   
   // constructor
-  function new (string name = "vseq_simple_test", 
+  function new (string name = "vseq_base_test", 
                 uvm_component parent = null);
     super.new(name, parent);
   endfunction
   
   // build phase
   virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    //vip_base_sequence::type_id::set_type_override(
-      //vip_simple_sequence::get_type());
-    set_type_override_by_type(vip_base_sequence::get_type(), 
-                            vip_simple_sequence::get_type());
-    // run the simple_vseq
-    //set_type_override_by_type(base_vseq::get_type(), 
-                              //simple_vseq::get_type());
-    // run the simple_reg_vseq
-    set_type_override_by_type(base_vseq::get_type(), 
-                              simple_reg_vseq::get_type());
-    
+    super.build_phase(phase);    
   endfunction
   
   // end of elaboration
@@ -201,3 +264,61 @@ class vseq_simple_test extends vip_base_test;
   endtask
   
 endclass
+
+class vseq_simple_test extends vseq_base_test;
+  
+  `uvm_component_utils(vseq_simple_test)
+  
+  function new (string name = "vseq_simple_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  virtual function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    //vip_base_sequence::type_id::set_type_override(
+      //vip_simple_sequence::get_type());
+    set_type_override_by_type(vip_base_sequence::get_type(), 
+                            vip_simple_sequence::get_type());
+    // run the simple_vseq
+    set_type_override_by_type(base_vseq::get_type(), 
+                              simple_vseq::get_type());
+    // run the simple_reg_vseq
+    //set_type_override_by_type(base_vseq::get_type(), 
+      //                        simple_reg_vseq::get_type());    
+  endfunction
+  
+endclass
+
+// c test
+class c_base_test extends vseq_base_test;
+    
+  `uvm_component_utils(c_base_test)
+  
+  function new (string name = "c_base_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  export "DPI-C" function send_tr;
+  
+  // only imported context tasks can call exported tasks
+  import "DPI-C" context task start_c();
+
+
+  // run phase
+  virtual task run_phase(uvm_phase phase);
+    phase.raise_objection(this, "test is raising an ojection");
+    
+    start_c();
+    
+    phase.drop_objection(this, "test is dropping an ojection");
+  endtask
+
+  
+  function void send_tr();
+    `uvm_info($sformatf("%s", this.get_name()), "Hello from SV", UVM_LOW)
+  endfunction
+  
+  
+endclass
+
+
